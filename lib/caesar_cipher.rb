2cbdac971812c672524e4a00af1eaebf796721a9
Ryan Wooff <ryanwooff@gmail.com>
2017-01-31 23:38:47 +0000

    initial commit

---
+++ lib/caesar_cipher.rb
@@ -0,0 +1,8 @@
+class String
+  def rot13
+    lowercase  = Array('a'..'z')
+    uppercase  = Array('A'..'Z')
+    cipher_txt = Hash[lowercase.zip(lowercase.rotate(13))].merge(Hash[uppercase.zip(uppercase.rotate(13))])
+    self.chars.map { |c| cipher_txt.fetch(c, c) }.join unless self.nil?
+  end
+end
