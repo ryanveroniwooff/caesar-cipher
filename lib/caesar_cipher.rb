18e14be3a11ccdd069e6f8b7df2990431ee4adf8
Ryan Wooff <ryanwooff@gmail.com>
2017-02-05 20:08:46 -0800

    Update caesar_cipher.rb

--- lib/caesar_cipher.rb
+++ lib/caesar_cipher.rb
@@ -3,6 +3,6 @@ class String
     lowercase  = Array('a'..'z')
     uppercase  = Array('A'..'Z')
     cipher_txt = Hash[lowercase.zip(lowercase.rotate(13))].merge(Hash[uppercase.zip(uppercase.rotate(13))])
-    self.chars.map { |c| cipher_txt[c] }.join unless self.nil?
+    self.chars.map { |c| cipher_txt.fetch(c,c) }.join unless self.nil?
   end
 end
